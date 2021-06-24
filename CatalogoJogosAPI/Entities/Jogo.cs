01f9d8294a877cae375a813429c96b885cbd4b56
Julio Cesar Iglesias <juliocesariglesias77@gmail.com>
2021-06-24 07:04:51 -0300

    criação da pasta Entities e da classe Jogo

---
+++ CatalogoJogosAPI/Entities/Jogo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoJogosAPI.Entities
+{
+    public class Jogo
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Fornecedora { get; set; }
+        public double Preco { get; set; }
+    }
+}
