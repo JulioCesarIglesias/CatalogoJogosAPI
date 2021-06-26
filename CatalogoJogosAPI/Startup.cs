7e5e2d2280b6636440788d40042e6cd4785e634e
Julio Cesar Iglesias <juliocesariglesias77@gmail.com>
2021-06-26 14:37:34 -0300

    adicionando as configurações do xml no swaggerGen

--- CatalogoJogosAPI/Startup.cs
+++ CatalogoJogosAPI/Startup.cs
@@ -12,7 +12,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace CatalogoJogosAPI
@@ -46,6 +48,9 @@ namespace CatalogoJogosAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogoJogosAPI", Version = "v1" });
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                c.IncludeXmlComments(Path.Combine(basePath, fileName));
             });
         }
 
